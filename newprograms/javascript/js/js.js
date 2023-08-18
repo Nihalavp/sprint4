13025065915edde14f3aacc2667e322b60baa27c
nihalavp <nihalavp42@gmail.com>
2023-08-18 11:55:21 +0530

    javascript

---
+++ newprograms/javascript/js/js.js
@@ -0,0 +1,2 @@
+console.log("helloooooo");
+document.write("<h1>welcome</h1>")
---
+++ sum.ts
@@ -0,0 +1,7 @@
+var a;
+var b;
+var sum;
+a=parseInt(prompt("enter a value"));
+b=parseInt(prompt("enter a value"));
+sum=a+b;
+console.log(sum);
